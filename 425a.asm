f55bbdd72a9a40df84ed459418dd94e16f994040
helsonxiao <helsonxiao@gmail.com>
2017-04-25 16:11:14 +0800

    new files and bugs fixed

---
+++ 425a.asm
@@ -0,0 +1,72 @@
+DATA    SEGMENT
+BUF1	DB 'Please input a letter:$'
+BUF2	DB 'The ASCII Code is:$'
+DATA    ENDS
+        ;
+STACK	SEGMENT PARA STACK  ; 仅能作为堆栈的逻辑段来使用
+        DB 100 DUP(0)
+STACK	ENDS
+        ;
+CODE	SEGMENT PARA PUBLIC 'CODE'
+        ASSUME CS:CODE, DS:DATA, SS:STACK
+START	PROC FAR    ; 远过程（调用程序不在同一个代码段内）
+        MOV AX, DATA
+        MOV DS, AX
+        MOV AX, STACK
+        MOV SS, AX
+        ; 输出BUF1
+        MOV AH, 09H
+        MOV DX, OFFSET BUF1
+        INT 21H  ; Interrupt DOS软中断功能调用
+        ; 获取字母
+        MOV AH, 1
+        INT 21H
+        ; 换行，输出BUF2
+        PUSH AX
+        CALL RTN  ; CPU 指令
+        MOV AH, 09H
+        MOV DX, OFFSET BUF2
+        INT 21H
+        POP AX	
+        ; 输出百位
+        MOV AH, 0
+        MOV BL, 100
+        DIV BL
+        PUSH AX
+        ADD AL, 30H
+        MOV DL, AL
+        INT 21H
+        POP AX
+        ; 输出十位
+        MOV AL, AH
+        MOV AH, 0
+        MOV BL, 10
+        DIV BL
+        PUSH AX
+        ADD AL, 30H
+        MOV DL, AL
+        MOV AH, 2
+        INT 21H
+        POP AX
+        ; 输出个位
+        ADD AH, 30H
+        MOV DL, AH
+        MOV AH, 2
+        INT 21H
+        ; 返回DOS提示符
+        MOV AH, 4CH
+        INT 21H
+START	ENDP
+        ;
+RTN	    PROC NEAR  ; 可以省略NEAR
+        MOV AH, 2
+        MOV DL, 0DH
+        INT 21H
+        MOV AH, 2
+        MOV DL, 0AH
+        INT 21H
+        RET  ; CPU 返回指令
+RTN	    ENDP  ; 伪指令
+        ;
+CODE	ENDS
+        END START
--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Just Code It
 
 ## Introduction
-As you can see, theses files are schoolworks.
+Theses files are schoolworks.
 No more details except explanations inside.
 
-*这里没有任何隐藏，看脸就可以了*
+*看脸就可以了*
